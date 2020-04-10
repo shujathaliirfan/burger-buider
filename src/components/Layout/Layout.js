eb28719ff3660159ff85a7634504806b66767250
shujathaliirfan <shujathali92@gmail.com>
2020-04-11 00:32:06 +0530

    initial commit

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Aux from '../../hoc/Aux'
+
+const layout = (props) =>(
+    <Aux>
+    <div >toolbar,side drawer,backdrop</div>
+    <main>
+    {props.children}
+    </main>
+        
+    </Aux>
+
+)
+
+export default layout;
